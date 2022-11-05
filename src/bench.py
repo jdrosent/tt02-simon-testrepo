6909a2478a05661859232d4a186203bb83833eee
Fraser Price <fraserbcprice@gmail.com>
2022-11-05 16:22:18 +0000

    Fixed testbench

--- src/bench.py
+++ src/bench.py
@@ -7,72 +7,79 @@ import ctypes
 import json
 
 with open("test_data.json", "r") as tfile:
-    test_data = json.load(tfile)
+	test_data = json.load(tfile)
 
 # Behavioural-Level Test
 @cocotb.test()
 async def test_bl(dut):
-    await test_simon(dut, gl=False)
+	await test_simon(dut, gl=False)
 
 # Gate-Level Test
 @cocotb.test()
-async def test_bl(dut):
-    await test_simon(dut, gl=True)
+async def test_gl(dut):
+	await test_simon(dut, gl=True)
 
 # Main Test
-async def test_simon(dut):
-    # Set values initially to 0
-    dut.i_shift.value = 0
-    dut.i_data.value = 0
+async def test_simon(dut, gl=False):
+	# Set values initially to 0
+	dut.i_shift.value = 0
+	dut.i_data.value = 0
 
-    # DUT Clock
-    cocotb.start_soon(Clock(dut.i_clk, 2, "ns").start())
-    await ClockCycles(dut.i_clk, 2)
-    
-    # Shift in key and plaintext
-    key = test_data["key"]
-    plaintext = test_data["plaintext"]
+	# DUT Clock
+	cocotb.start_soon(Clock(dut.i_clk, 2, "ns").start())
+	await ClockCycles(dut.i_clk, 2)
+	
+	# Shift in key and plaintext
+	key = test_data["key"]
+	plaintext = test_data["plaintext"]
 
-    dut.i_shift.value = 1
-    await ClockCycles(dut.i_clk, 1)
+	dut.i_shift.value = 1
+	await ClockCycles(dut.i_clk, 1)
 
-    for x in range(8):
-        dut.i_data.value = (plaintext >> (4*x)) & 0x0F
-        await ClockCycles(dut.i_clk, 1)
+	for x in range(8):
+		dut.i_data.value = (plaintext >> (4*x)) & 0x0F
+		await ClockCycles(dut.i_clk, 1)
 
-    for x in range(16):
-        dut.i_data.value = (key >> (4*x)) & 0x0F
-        await ClockCycles(dut.i_clk, 1)
-    
-    dut.i_shift.value = 0
+	for x in range(16):
+		dut.i_data.value = (key >> (4*x)) & 0x0F
+		await ClockCycles(dut.i_clk, 1)
+	
+	dut.i_shift.value = 0
 
-    # Check that rounds and key expansions match expected
-    keys = test_data["keys"]
-    rounds = test_data["rounds"]
-    print("Key (Python)      Key (Verilog)     Round (Python)  Round (Verilog)")
-    print("------------------------------------------------------------------")
-    for x in range(len(keys)-1):
-        await ClockCycles(dut.i_clk, 1)
+	# Read key and round values from test data
+	keys = test_data["keys"]
+	rounds = test_data["rounds"]
 
-        dut_key = dut.simon0.r_key.value
-        dut_round = dut.simon0.r_round.value
+	# Check round and key expansions match expected
+	if not gl:
+		print("Key (Python)      Key (Verilog)     Round (Python)  Round (Verilog)")
+		print("------------------------------------------------------------------")
+		for x in range(len(keys)-1):
+			await ClockCycles(dut.i_clk, 1)
 
-        print(f"{keys[x]:016x}  {dut_key.integer:016x}  {rounds[x]:08x}        {dut_round.integer:08x}")
-        assert(dut_key == keys[x])
-        assert(dut_round == rounds[x])
+			dut_key = dut.simon0.r_key.value
+			dut_round = dut.simon0.r_round.value
 
-    # Shift out the ciphertext
-    dut.i_shift.value = 1
-    await ClockCycles(dut.i_clk, 1)
+			print(f"{keys[x]:016x}  {dut_key.integer:016x}  {rounds[x]:08x}        {dut_round.integer:08x}")
+			assert(dut_key == keys[x])
+			assert(dut_round == rounds[x])
+	else:
+		await ClockCycles(dut.i_clk, len(keys)-1)
 
-    dut_key = dut.simon0.r_key.value
+	# Shift out the ciphertext
+	dut.i_shift.value = 1
+	await ClockCycles(dut.i_clk, 1)
 
-    ciphertext = 0
-    for x in range(8):
-        ciphertext += dut.o_data.value << (4*x)
-        await ClockCycles(dut.i_clk, 1)
+	if not gl:
+		dut_key = dut.simon0.r_key.value
 
-    print(f"{keys[-1]:016x}  {dut_key.integer:016x}  {rounds[-1]:08x}        {ciphertext:08x}")
+	ciphertext = 0
+	for x in range(8):
+		ciphertext += dut.o_data.value << (4*x)
+		await ClockCycles(dut.i_clk, 1)
 
-    assert(keys[-1] == dut_key.integer)
-    assert(rounds[-1] == ciphertext)
+	if not gl:
+		print(f"{keys[-1]:016x}  {dut_key.integer:016x}  {rounds[-1]:08x}        {ciphertext:08x}")
+		assert(keys[-1] == dut_key.integer)
+
+	assert(rounds[-1] == ciphertext)
--- src/tb.v
+++ src/tb.v
@@ -8,20 +8,35 @@ module tb (
 	o_data
 );
 
+	/* Generate VCD dump */
 	initial begin
 		$dumpfile ("tb.vcd");
 		$dumpvars (0, tb);
 		#1;
 	end
 
+	/* Module Interface */
 	input  wire       i_clk;
 	input  wire       i_shift;
 	input  wire [3:0] i_data;
 	output wire [3:0] o_data;
 
+	`ifdef GL_TEST
+	/* Gate-Level Sim */
 	fraserbc_simon simon0 (
 		.io_in({2'b00,i_data,i_shift,i_clk}),
-		.io_out({o_data})
+		.io_out({o_data}),
+		.vccd1(1'b1),
+        .vssd1(1'b0)
 	);
+	`else
+	/* Behavioural-Level Sim */
+	simon simon0 (
+		.i_clk(i_clk),
+		.i_shift(i_shift),
+		.i_data(i_data),
+		.o_data(o_data)
+	);
+	`endif
 
 endmodule
